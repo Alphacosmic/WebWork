0878ab4c30bf57aeb5e99d2981746179c4013f0f
SPARTACUS5329 <adityaspartacus5329@gmail.com>
2022-09-20 23:08:02 +0530

    slot vacancy done

--- src/views/StudentDashboard/Profiles/Slot.jsx
+++ src/views/StudentDashboard/Profiles/Slot.jsx
@@ -1,18 +1,27 @@
 import React from "react";
-import { Button } from "antd";
+import { Button, Popover } from "antd";
 
 function Slot(props) {
 	const { slot, setSelectedSlot } = props.props;
+
+	const occupantContact = <div>{slot.applicant?.email || "Vacant"}</div>;
+
 	return (
 		<div style={{ marginRight: "10px" }}>
-			<Button
-				style={{ borderRadius: "30px" }}
-				onClick={(e) => {
-					e.preventDefault();
-					setSelectedSlot(slot.timeRange);
-				}}>
-				{slot.timeRange}
-			</Button>
+			<Popover content={occupantContact}>
+				<Button
+					style={{
+						borderRadius: "30px",
+						color: slot.status === "BOOKED" ? "#a83240" : "#018711",
+					}}
+					disabled={slot.status === "BOOKED"}
+					onClick={(e) => {
+						e.preventDefault();
+						setSelectedSlot(slot.timeRange);
+					}}>
+					{slot.timeRange}
+				</Button>
+			</Popover>
 		</div>
 	);
 }
