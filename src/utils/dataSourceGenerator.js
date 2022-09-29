6c46fe51061b577b868df30b476b576c59f06a49
SPARTACUS5329 <adityaspartacus5329@gmail.com>
2022-09-29 18:53:24 +0530

    changed jsdoc

--- src/utils/dataSourceGenerator.js
+++ src/utils/dataSourceGenerator.js
@@ -6,7 +6,7 @@ import sortByNumberOfApplicants from "./sortByNumberOfApplicants";
  * Sorts the project array by date of creation
  * @param {array} profiles - The profiles/ profiles array to be sorted.
  * @param {('ALL'|'APPLIED'|'SELECTED'|'REJECTED'|'ELIGIBLE')} sort - The type of filtering.
- * @param {('LATEST_FIRST'|'OLDEST_FIRST')} sort - The type of sorting.
+ * @param {('STIPEND'|'NUMBER_OF_APPLICANTS')} sort - The type of sorting.
  * @param {('NONE'|'ASCENDING'|'DESCENDING')} order - The order of sorting
  */
 
